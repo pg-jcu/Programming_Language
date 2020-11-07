1ccad3b668f04a75d528b8b4687ea909761025cc
nigercharta <chan3358@naver.com>
2020-11-07 13:31:30 +0900

    add chapter08

---
+++ java_study/src/chapter08/Ex02.java
@@ -0,0 +1,26 @@
+package chapter08;
+import java.io.*;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		InputStreamReader in = null;
+		FileInputStream fin = null;
+		try {
+			fin = new FileInputStream("c:\\Temp\\hangul.txt");
+			in = new InputStreamReader(fin, "UTF-8");
+			int c;
+			
+			System.out.println("인코딩 문자 집합은 " + in.getEncoding());
+			while ((c = in.read()) != -1) {
+				System.out.print((char)c);
+			}
+			in.close();
+			fin.close();
+		} catch (IOException e) {
+			System.out.println("입출력 오류");
+		}
+
+	}
+
+}
