b7aabd09407cb820972fcd3869044de82208a11f
unexistentman <chan3358@naver.com>
2021-07-02 10:31:27 +0900

    add

---
+++ average.c
@@ -0,0 +1,21 @@
+/* 세 숫자들을 두 개 씩 짝지은 모든 경우의 평균을 구한다 */
+
+#include <stdio.h>
+
+double get_average(double left, double right) {
+    return (left + right) / 2;
+}
+
+int main(void) {
+    double x;
+    double y;
+    double z;
+
+    printf("숫자 세 개를 입력해주세요: ");
+    scanf("%lf%lf%lf", &x, &y, &z);
+    printf("%g와 %gd의 평균: %g\n", x, y, get_average(x, y));
+    printf("%g와 %gd의 평균: %g\n", y, z, get_average(y, z));
+    printf("%g와 %gd의 평균: %g\n", z, x, get_average(z, x));
+
+    return 0;
+}
