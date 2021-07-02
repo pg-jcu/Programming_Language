acd61eddf3dd6c402a440ed61835e0f0789c87a4
unexistentman <chan3358@naver.com>
2021-07-02 10:34:45 +0900

    add

---
+++ countdown.c
@@ -0,0 +1,16 @@
+/* 카운트다운을 출력함 */
+
+#include <stdio.h>
+
+void print_count(int number) {
+    printf("T 빼기 %d, 세는 중\n", number);
+}
+
+int main(void) {
+    int i;
+    for (i = 10; i > 0; --i) {
+        print_count(i);
+    }
+
+    return 0;
+}
