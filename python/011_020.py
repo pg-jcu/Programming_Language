74c5011ce3421bb47dd07dc30da69c28f0438a74
unexistentman <chan3358@naver.com>
2021-09-04 23:57:38 +0900

    add

---
+++ python/011_020.py
@@ -0,0 +1,52 @@
+# 011
+samsung_elec = 50000
+valuation_amount = samsung_elec * 10
+print(valuation_amount)
+
+# 012
+market_cap = 298000000000000
+current_price = 50000
+PER = 15.79
+print(market_cap, type(market_cap))
+print(current_price, type(current_price))
+print(PER, type(PER))
+
+# 013
+s = "hello"
+t = "python"
+print(s + '!', t)
+
+# 014
+print(2 + 2 * 3)
+
+# 015
+a = "132"
+print(type(a))
+
+# 016
+num_str = "720"
+num_int = int(num_str)
+print(num_int, type(num_int))
+
+# 017
+num = 100
+num_str = str(num)
+print(num_str, type(num_str))
+
+# 018
+num_str = "15.79"
+num_float = float(num_str)
+print(num_float, type(num_float))
+
+# 019
+year = "2020"
+year_int = int(year)
+print(year_int - 3, type(year_int))
+print(year_int - 2, type(year_int))
+print(year_int - 1, type(year_int))
+
+# 020
+month_cost = 48584
+installment = 36
+total_cost = month_cost * installment
+print(total_cost)
