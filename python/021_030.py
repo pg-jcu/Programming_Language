e3f3b715d36271710c560352bbcdebc037876dd1
unexistentman <chan3358@naver.com>
2021-09-05 13:20:57 +0900

    add

---
+++ python/021_030.py
@@ -0,0 +1,42 @@
+# 021
+letters = 'python'
+print(letters[0], letters[2])
+
+# 022
+license_plate = "24가 2210"
+print(license_plate[-4:])
+
+# 023
+string = "홀짝홀짝홀짝"
+print(string[::2])
+
+# 024
+string = "PYTHON"
+print(string[::-1])
+
+# 025
+phone_number = "010-1111-2222"
+print(phone_number.replace('-', ' '))
+
+# 026
+phone_number = "010-1111-2222"
+print(phone_number.replace('-', ''))
+
+# 027
+url = "http://sharebook.kr"
+url_split = url.split('.')
+print(url_split[-1])
+
+# 028
+lang = 'python'
+# lang[0] = 'P' # 문자열은 수정 불가
+print(lang) 
+
+# 029
+string = 'abcdfe2a354a32a'
+print(string.replace('a', 'A'))
+
+# 030
+string = 'abcd'
+string.replace('b', 'B')
+print(string)
