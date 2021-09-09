7af91752ec368c805f04ba1fba65792d04e30e38
unexistentman <chan3358@naver.com>
2021-09-09 17:21:29 +0900

    add

---
+++ python/초보자를_위한_파이썬_300제/131_140.py
@@ -0,0 +1,57 @@
+# 131
+fruits = ["사과", "귤", "수박"]
+for i in fruits:
+    print(i)
+
+# 132
+for i in fruits:
+    print("#####")
+
+# 133 
+i = 'A'
+print(i)
+i = 'B'
+print(i)
+i = 'C'
+print(i)
+
+# 134
+i = 'A'
+print("출력:", i)
+i = 'B'
+print("출력:", i)
+i = 'C'
+print("출력:", i)
+
+# 135
+i = 'A'
+b = i.lower()
+print("변환:", b)
+i = 'B'
+b = i.lower()
+print("변환:", b)
+i = 'C'
+b = i.lower()
+print("변환:", b)
+
+# 136
+for i in [10, 20, 30]:
+    print(i)
+
+# 137
+for i in [10, 20, 30]:
+    print(i)
+
+# 138
+for i in [10, 20, 30]:
+    print(i)
+    print("-------")
+
+# 139
+print("++++")
+for i in [10, 20, 30]:
+    print(i)
+
+# 140
+for _ in range(4):
+    print("-------")
