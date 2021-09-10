57d3850122b4c6a303518179378e7abb5989f644
unexistentman <chan3358@naver.com>
2021-09-10 14:36:10 +0900

    add

---
+++ python/초보자를_위한_파이썬_300제/161_170.py
@@ -0,0 +1,45 @@
+# 161
+for i in range(100):
+    print(i)
+
+# 162
+for i in range(2002, 2051, 4):
+    print(i)
+
+# 163
+for i in range(3, 31, 3):
+    print(i)
+
+# 164
+for i in range(99, 0, -1):
+    print(i)
+
+# 165
+for i in range(10):
+    print(float(i/10))
+
+# 166
+for i in range(1, 10):
+    print("3x{} = {}".format(i, 3 * i))
+
+# 167
+for i in range(1, 10, 2):
+    print("3x{} = {}".format(i, 3 * i))
+
+# 168
+num = 0
+for i in range(1, 11):
+    num += i
+print("합 :", num)
+
+# 169
+num = 0
+for i in range(1, 10, 2):
+    num += i
+print("합:", num)
+
+# 170
+num = 1
+for i in range(1, 11):
+    num *= i
+print(num)
