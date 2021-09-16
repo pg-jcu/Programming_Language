3914d66fb881e37ac3a64296d035103c951830dc
unexistentman <chan3358@naver.com>
2021-09-16 19:21:18 +0900

    add

---
+++ python/점프_투_파이썬/sub_dir_search.py
@@ -0,0 +1,23 @@
+import os
+
+def search(dirname):
+    try:
+        filenames = os.listdir(dirname)
+        for filename in filenames:
+            full_filename = os.path.join(dirname, filename)
+            if os.path.isdir(full_filename):
+                search(full_filename)
+            else:
+                ext = os.path.splitext(full_filename)[1]
+                if ext == '.py':
+                    print(full_filename)
+    except PermissionError:
+        pass
+
+search("C:/")
+
+# for (path, dir, files) in os.walk("C:/"):
+#     for filename in files:
+#         ext = os.path.splitext(filename)[-1]
+#         if ext == '.py':
+#             print(f"{path}/{filename}")
