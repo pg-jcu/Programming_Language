13a5635c8b4681ec10d7965d376f676398a1dd55
unexistentman <chan3358@naver.com>
2021-09-17 13:42:11 +0900

    add

---
+++ python/파이썬_라이브러리/03/graphlib_TopologicalSorter.py
@@ -0,0 +1,14 @@
+# python 3.9 above
+# from graphlib import TopologicalSorter
+
+# ts = TopologicalSorter()
+
+# ts.add('영어중급', '영어초급')  
+# ts.add('영어고급', '영어중급')
+
+# ts.add('영어문법', '영어중급') 
+# ts.add('영어고급', '영어문법')  
+
+# ts.add('영어회화', '영어문법')
+
+# print(list(ts.static_order()))
