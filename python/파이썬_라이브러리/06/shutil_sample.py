bf245dbb09e40f12332077025b0c2ea50a5599de
unexistentman <chan3358@naver.com>
2021-09-23 13:04:31 +0900

    add

---
+++ python/파이썬_라이브러리/06/shutil_sample.py
@@ -0,0 +1,5 @@
+import shutil
+
+shutil.copy("C:/Users/jeong/Downloads/saying.txt", "C:/Users/jeong/saying.txt.bak")
+
+shutil.move("C:/Users/jeong/Downloads/saying.txt", "C:/Users/jeong/saying.txt")
