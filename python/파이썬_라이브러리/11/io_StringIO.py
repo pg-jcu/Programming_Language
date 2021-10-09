d61f399f80b2996ce03e97c7656e94ebd8797be6
unexistentman <chan3358@naver.com>
2021-10-09 23:16:58 +0900

    add

---
+++ python/파이썬_라이브러리/11/io_StringIO.py
@@ -0,0 +1,23 @@
+import csv
+import io
+
+def execute(f):
+    result = []
+    reader = csv.reader(f)
+    for line in reader:
+        one = int(line[0])
+        two = int(line[1])
+        three = one + two
+        line.append(three)
+        result.append(line)
+    return result
+
+src = '''\
+20,40
+50,90
+77,22
+'''
+
+with io.StringIO(src) as f:
+    result = execute(f)
+    print(result)
