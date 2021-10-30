c79575855748a2bd61711e130c93ecee25cb73ee
unexistentman <chan3358@naver.com>
2021-10-30 22:56:21 +0900

    add

--- javascript/모던_자바스크립트_Deep_Dive/37/3720.js
+++ javascript/모던_자바스크립트_Deep_Dive/37/3720.js
@@ -1,16 +1,16 @@
-// Set.prototype.union = function (set) {
-//     const result = new Set(this);
-
-//     for (const value of set) {
-//         result.add(value);
-//     }
-
-//     return result;
-// };
-
 Set.prototype.union = function (set) {
-    return new Set([...this, ...set]);
+    const result = new Set(this);
+
+    for (const value of set) {
+        result.add(value);
+    }
+
+    return result;
 };
+
+// Set.prototype.union = function (set) {
+//     return new Set([...this, ...set]);
+// };
 
 const setA = new Set([1, 2, 3, 4]);
 const setB = new Set([2, 4]);
