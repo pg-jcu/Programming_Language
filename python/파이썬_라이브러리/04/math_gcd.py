6eff6878c491a5bbb02bf55d88c16d3e913c5f91
unexistentman <chan3358@naver.com>
2021-11-08 16:32:52 +0900

    modify

--- python/파이썬_라이브러리/04/math_gcd.py
+++ python/파이썬_라이브러리/04/math_gcd.py
@@ -3,4 +3,4 @@ import math
 # gcd : greatest common divisor
 print(math.gcd(60, 100))
 
-# print(math.gcd(60, 100, 80)) # version 3.9 above
+print(math.gcd(60, 100, 80)) # version 3.9 above
--- python/파이썬_라이브러리/04/math_lcm.py
+++ python/파이썬_라이브러리/04/math_lcm.py
@@ -1,4 +1,4 @@
 import math
 
 # lcm : least common multiple
-# print(math.lcm(15, 25)) # version 3.9 above
+print(math.lcm(15, 25)) # version 3.9 above
