5861e1627214cccd3ef22ebe0fc74a29250479d2
unexistentman <chan3358@naver.com>
2021-11-26 22:06:04 +0900

    modify

--- react/리액트를_다루는_기술/08/08_04_00.js
+++ react/리액트를_다루는_기술/08/08_04_00.js
@@ -3,7 +3,7 @@ import React, { useState, useMemo } from 'react';
 const getAverage = numbers => {
     console.log('평균값 계산 중...');
     if (numbers.length === 0) return 0;
-    const sum = numbers.reduce((a, b) => a + b);
+    const sum = numbers.reduce((a, b) => a + b, 0);
     return sum / numbers.length;
 };
 
@@ -14,7 +14,7 @@ const Average = () => {
     const onChange = e => {
         setNumber(e.target.value);
     };
-    const onInsert = e => {
+    const onInsert = () => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
