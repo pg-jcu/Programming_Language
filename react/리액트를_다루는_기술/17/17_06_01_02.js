026c0ffa1b6f4d2964bbb8fcaec5d02210e59ba6
unexistentman <chan3358@naver.com>
2021-12-21 22:37:50 +0900

    add

--- react/리액트를_다루는_기술/17/17_06_01_02.js
+++ react/리액트를_다루는_기술/17/17_06_01_02.js
@@ -36,20 +36,36 @@ const initialState = {
 
 const todos = handleActions(
     {
-        [CHANGE_INPUT]: (state, action) => ({ ...state, input: action.payload }),
-        [INSERT]: (state, action) => ({
+        // [CHANGE_INPUT]: (state, action) => ({ ...state, input: action.payload }),
+        // [INSERT]: (state, action) => ({
+        //     ...state,
+        //     todos: state.todos.concat(action.payload)
+        // }),
+        // [TOGGLE]: (state, action) => ({
+        //     ...state,
+        //     todos: state.todos.map(todo =>
+        //         todo.id === action.payload ? { ...todo, done: !todo.done } : todo    
+        //     )
+        // }),
+        // [REMOVE]: (state, action) => ({
+        //     ...state,
+        //     todos: state.todos.filter(todo => todo.id !== action.payload)
+        // })
+
+        [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+        [INSERT]: (state, { payload: todo }) => ({
             ...state,
-            todos: state.todos.concat(action.payload)
+            todos: state.todos.concat(todo)
         }),
-        [TOGGLE]: (state, action) => ({
+        [TOGGLE]: (state, { payload: id }) => ({
             ...state,
             todos: state.todos.map(todo =>
-                todo.id === action.payload ? { ...todo, done: !todo.done } : todo    
+                todo.id === id ? { ...todo, done: !todo.done } : todo    
             )
         }),
-        [REMOVE]: (state, action) => ({
+        [REMOVE]: (state, { payload: id }) => ({
             ...state,
-            todos: state.todos.filter(todo => todo.id !== action.payload)
+            todos: state.todos.filter(todo => todo.id !== id)
         })
     },
     initialState
