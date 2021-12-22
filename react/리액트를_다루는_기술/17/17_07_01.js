b91f6dbfb0442d7eab62da3e659dc67608cbfe0c
unexistentman <chan3358@naver.com>
2021-12-22 22:25:13 +0900

    add

---
+++ react/리액트를_다루는_기술/17/17_07_01.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Counter from '../components/Counter';
+
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    return (
+        <Counter number={number} />
+    );
+};
+
+export default CounterContainer;
