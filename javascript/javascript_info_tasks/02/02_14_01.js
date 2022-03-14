7d8d33bb58b512194438aa3e140449cdf54e6afd
unexistentman <chan3358@naver.com>
2022-03-14 22:06:06 +0900

    add

---
+++ javascript/javascript_info_tasks/02/02_14_01.js
@@ -0,0 +1,14 @@
+// Rewrite the "switch" into an "if"
+
+let browser;
+
+if (browser === 'Edge') {
+    console.log("You've got the Edge!");
+} else if (browser === 'Chrome'
+        || browser === 'Firefox'
+        || browser === 'Safari'
+        || browser === 'Opera') {
+    console.log('Okay we support these browsers too');
+} else {
+    console.log('We hope that this page looks ok!');
+}
