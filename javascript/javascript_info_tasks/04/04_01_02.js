07cb22acd112b033c8bf4690c1d7b9ee0264a84f
unexistentman <chan3358@naver.com>
2022-04-28 20:55:40 +0900

    add

---
+++ javascript/javascript_info_tasks/04/04_01_02.js
@@ -0,0 +1,16 @@
+// Check for emptiness
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    } 
+    return true; 
+}
+
+let schedule = {};
+
+console.log( isEmpty(schedule) );
+
+schedule["8:30"] = "get up";
+
+console.log( isEmpty(schedule) ); 
