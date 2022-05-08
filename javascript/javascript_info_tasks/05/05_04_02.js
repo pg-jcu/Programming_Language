659c1dcc33c3b232ebc46df9bbadc868d36fe26d
unexistentman <chan3358@naver.com>
2022-05-08 22:57:03 +0900

    add

---
+++ javascript/javascript_info_tasks/05/05_04_02.js
@@ -0,0 +1,11 @@
+// Array operations
+
+let styles = ["Jazz", "Blues"];
+
+styles.push("Rock-n-Roll");
+
+styles[Math.floor((styles.length - 1) / 2)] = "Classics";
+
+console.log(styles.shift());
+
+styles.unshift("Rap", "Reggae");
