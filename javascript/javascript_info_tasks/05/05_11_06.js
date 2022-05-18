f609e64b214dfbec7b5754247b66209a0452da80
unexistentman <chan3358@naver.com>
2022-05-18 17:46:31 +0900

    add

---
+++ javascript/javascript_info_tasks/05/05_11_06.js
@@ -0,0 +1,17 @@
+// How many seconds have passed today?
+
+function getSecondsToday() {
+    let now = new Date();
+    let today = new Date();
+
+    return Math.round((now - today.setHours(0, 0, 0, 0)) / 1000);
+}
+
+function getSecondsToday() {
+    let now = new Date();
+  
+    let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  
+    let diff = now - today;
+    return Math.round(diff / 1000);
+}
