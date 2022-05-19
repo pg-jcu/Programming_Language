465009e0f26f3b5a919f5113439ad9bd87fe8a11
unexistentman <chan3358@naver.com>
2022-05-19 11:08:59 +0900

    modify

--- javascript/javascript_info_tasks/05/05_11_08.js
+++ javascript/javascript_info_tasks/05/05_11_08.js
@@ -7,8 +7,8 @@ function formatDate(date) {
     let hour = date.getHours();
     let minutes = date.getMinutes();
     let diffMs = new Date() - date;
-    let diffSec = Math.round(diffMs / 1000);
-    let diffMin = diffSec / 60;
+    let diffSec = Math.floor(diffMs / 1000);
+    let diffMin = Math.floor(diffSec / 60);
     let diffHour = diffMin / 60;
 
     year = year.toString().slice(-2);
@@ -27,33 +27,3 @@ function formatDate(date) {
         return `${dayOfMonth}.${month}.${year} ${hour}:${minutes}`
       }
 }
-
-// function formatDate(date) {
-//     let diff = new Date() - date;
-  
-//     if (diff < 1000) {
-//       return 'right now';
-//     }
-  
-//     let sec = Math.floor(diff / 1000);
-  
-//     if (sec < 60) {
-//       return sec + ' sec. ago';
-//     }
-  
-//     let min = Math.floor(diff / 60000);
-//     if (min < 60) {
-//       return min + ' min. ago';
-//     }
-  
-//     let d = date;
-//     d = [
-//       '0' + d.getDate(),
-//       '0' + (d.getMonth() + 1),
-//       '' + d.getFullYear(),
-//       '0' + d.getHours(),
-//       '0' + d.getMinutes()
-//     ].map(component => component.slice(-2));
-  
-//     return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
-// }
