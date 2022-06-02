d714f72ec482a5684fdcd2a7aaec00ad22f9288a
butterstrawberry <chan3358@naver.com>
2022-06-02 10:21:37 +0900

    add

---
+++ javascript/javascript_info_tasks/06/06_10_05.js
@@ -0,0 +1,23 @@
+// Partial application for login
+
+function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+
+    if (password == "rockstar") {
+        ok();
+    } else {
+        fail();
+    }
+};
+
+let user = {
+    name: 'John',
+
+    login(result) {
+        alert(this.name + (result ? ' logged in' : ' failed to log in'));
+    }
+};
+
+askPassword(user.login.bind(user, true), user.login.bind(user, false));
+
+// askPassword(() => user.login(true), () => user.login(false));
