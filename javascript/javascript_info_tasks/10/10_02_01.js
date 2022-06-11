f37366236a543fa3e5425d32b06b2c471df10246
butterstrawberry <chan3358@naver.com>
2022-06-11 10:43:59 +0900

    add

---
+++ javascript/javascript_info_tasks/10/10_02_01.js
@@ -0,0 +1,18 @@
+// Inherit from SyntaxError
+
+class FormatError extends SyntaxError {
+    constructor(message) {
+        super(message);
+        // this.name = "FormatError";
+        this.name = this.constructor.name;
+    }
+}
+
+let err = new FormatError("formatting error");
+
+console.log(err.message);
+console.log(err.name);
+console.log(err.stack);
+
+console.log(err instanceof FormatError);
+console.log(err instanceof SyntaxError);
