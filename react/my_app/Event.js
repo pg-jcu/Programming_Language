f319c5e55ebdef2a7c0274826a87beee2cac5350
unexistentman <chan3358@naver.com>
2022-09-04 17:31:58 +0900

    add

---
+++ react/my_app/Event.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import './css/Event.css';
+
+function Event() {
+  const [form, setForm] = useState({
+    username: '',
+    message: ''
+  });
+  const {username, message} = form;
+  const onChange = event => {
+    const nextFrom = {
+      ...form,
+      [event.target.name]: event.target.value
+    };
+    setForm(nextFrom);
+  };
+  const onClick = () => {
+    alert(`${username}: ${message}`);
+    setForm({
+      username: '',
+      message: ''
+    })
+  };
+  const onKeyDown = event => {
+    if (event.key == 'Enter') {
+      onClick();
+    }
+  }
+
+  return (
+    <div className="event">
+      <input
+        type="text"
+        name="username"
+        placeholder="username"
+        value={username}
+        onChange={onChange}
+      />
+      <input 
+        type="text" 
+        name="message" 
+        placeholder="message"
+        value={message}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+      />  
+      <button onClick={onClick}>alert</button>
+    </div>
+  );
+}
+
+export default Event;
