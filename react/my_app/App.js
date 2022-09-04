20d639e0bfcd226f0665888f977e6c12be510179
unexistentman <chan3358@naver.com>
2022-09-04 22:35:20 +0900

    modify

--- react/my_app/App.js
+++ react/my_app/App.js
@@ -1,6 +1,8 @@
 import MyComponent from './MyComponent';
 import Counter from './Counter';
 import Event from './Event';
+import Validation from './Validation';
+import ScrollBox from './ScrollBox';
 import './css/App.css';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <MyComponent name='Jeong' number={1}>children</MyComponent>
       <Counter className='counter' />
       <Event />
+      <Validation />
+      <ScrollBox />
     </div>
   );
 }
