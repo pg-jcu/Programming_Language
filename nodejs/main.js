032ac3e6bca2a29999a8e7d2275c49e33e7f64e7
chan uk jeong <40534414+butterstrawberry@users.noreply.github.com>
2022-09-16 17:13:40 +0900

    modify

--- nodejs/main.js
+++ nodejs/main.js
@@ -7,43 +7,65 @@ let app = http.createServer((request, response) => {
   const _url = request.url;
   const __dirname = path.resolve();
   const queryData = url.parse(_url, true).query;
+  const pathname = url.parse(_url, true).pathname;
   let title = queryData.id;
 
-  if (_url == '/') {
-    title = 'WEB';
-  }
-
-  if (_url == '/favicon.ico') {
+  if (pathname === '/') {
+    if (title === undefined) {
+      title = 'Welcome';
+      const description = 'Hello, Node.js';
+      const template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=Javascript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+      `;
+  
+      response.writeHead(200);
+      response.end(template);
+    } else {
+      fs.readFile(`data/${title}`, 'utf8', (err, description) => {
+        const template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=Javascript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+        `;
+    
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+  } else {
     response.writeHead(404);
-    response.end();
-    return;
+    response.end('Not found');
   }
-
-  response.writeHead(200);
-
-  fs.readFile(`data/${title}`, 'utf8', (err, description) => {
-    const template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=Javascript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-    `;
-
-    response.end(template);
-  });
 });
 
 app.listen(3000);
