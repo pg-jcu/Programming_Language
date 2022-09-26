6d433b7a39a97e7c7b867de2d104cb74b32747db
chan uk jeong <40534414+butterstrawberry@users.noreply.github.com>
2022-09-26 22:14:52 +0900

    modify

--- nodejs/study_from_internet/main.js
+++ nodejs/study_from_internet/main.js
@@ -3,6 +3,31 @@ import fs from 'fs';
 import path from 'path';
 import url from 'url';
 
+function templateHTML(title, list, body) {
+  return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${body}
+    </body>
+    </html>
+  `;
+}
+
+function templateList(filelist) {
+  const list = `<ul>${filelist.map(file => {
+    return `<li><a href="/?id=${file}">${file}</a></li>`;
+  }).join('')}</ul>`;
+
+  return list;
+}
+
 let app = http.createServer((request, response) => {
   const _url = request.url;
   const __dirname = path.resolve();
@@ -14,51 +39,18 @@ let app = http.createServer((request, response) => {
       fs.readdir('./data', (err, filelist) => {
         const title = 'Welcome';
         const description = 'Hello, Node.js';
-        const list = `<ul>${filelist.map(file => {
-          return `<li><a href="/?id=${file}">${file}</a></li>`;
-        }).join('')}</ul>`;
-        const template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-        `;
+        const list = templateList(filelist);
+        const template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
     
         response.writeHead(200);
         response.end(template);
       });
     } else {
       fs.readdir('./data', (err, filelist) => {
-        const list = `<ul>${filelist.map(file => {
-          return `<li><a href="/?id=${file}">${file}</a></li>`;
-        }).join('')}</ul>`;
-
         fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
           const title = queryData.id;
-          const template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-          `;
+          const list = templateList(filelist);
+          const template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
       
           response.writeHead(200);
           response.end(template);
