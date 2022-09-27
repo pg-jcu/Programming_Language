56222cd7a545371b3fc3b20a049c1ea909aaddb5
chan uk jeong <40534414+butterstrawberry@users.noreply.github.com>
2022-09-27 17:15:13 +0900

    modify

--- nodejs/study_from_internet/main.js
+++ nodejs/study_from_internet/main.js
@@ -57,7 +57,12 @@ const app = http.createServer((request, response) => {
           const list = templateList(filelist);
           const template = templateHTML(title, list, 
             `<h2>${title}</h2><p>${description}</p>`,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            `<a href="/create">create</a> 
+             <a href="/update?id=${title}">update</a>
+             <form action="/delete_process" method="post">
+              <input type="hidden" name="id" value="${title}" />
+              <input type="submit" value="delete" />
+             </form>`
           );
       
           response.writeHead(200);
