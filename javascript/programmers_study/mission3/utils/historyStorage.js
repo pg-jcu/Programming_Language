9bef53f3b50c6f5107ed9c9113432202201bc0fe
chan uk jeong <40534414+butterstrawberry@users.noreply.github.com>
2022-09-29 19:14:52 +0900

    add

---
+++ javascript/programmers_study/mission3/utils/historyStorage.js
@@ -0,0 +1,23 @@
+export const getItem = (key, defaultValue) => {
+  try {
+    const storedValue = localStorage.getItem(key);
+
+    if (storedValue) {
+      return JSON.parse(storedValue);
+    }
+
+    return defaultValue;
+  } catch (e) {
+    alert('Data load failed!');
+
+    return defaultValue;
+  }
+};
+
+export const setItem = (key, value) => {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    alert('There is a problem with the data!');
+  }
+};
