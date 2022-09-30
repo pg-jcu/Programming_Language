29cb9eef914bd4968ad9007ed5166719023513a9
chan uk jeong <40534414+butterstrawberry@users.noreply.github.com>
2022-09-30 16:04:52 +0900

    modify

--- nodejs/express/index.js
+++ nodejs/express/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const fs = require('fs');
+const template = require('./lib/template.js');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  fs.readdir('./data', (error, filelist) => {
+    const title = 'Welcome';
+    const description = 'Hello, Express!';
+    const list = template.list(filelist);
+    const html = template.html(title, list,
+      `<h2>${title}</h2>${description}`,
+      ''
+    );
+    res.send(html);
+  });
 });
 
 app.listen(port, () => {
