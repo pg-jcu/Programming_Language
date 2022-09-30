f723818ec22db145b0946fde2ffc34b5fcae6962
chan uk jeong <40534414+butterstrawberry@users.noreply.github.com>
2022-09-30 16:45:52 +0900

    modify

--- nodejs/express/index.js
+++ nodejs/express/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const template = require('./lib/template.js');
 
 const app = express();
@@ -12,9 +13,31 @@ app.get('/', (req, res) => {
     const list = template.list(filelist);
     const html = template.html(title, list,
       `<h2>${title}</h2>${description}`,
-      ''
+      `<a href="/create">create</a>`
     );
     res.send(html);
+  });
+});
+
+app.get('/page/:pageId', (req, res) => {
+  fs.readdir('./data', (error, filelist) => {
+    const filteredId = path.parse(req.params.pageId);
+    fs.readFile(`/data/${filteredId}`, 'utf8', (err, description) => {
+      const title = req.params.pageId;
+      const list = template.list(filelist);
+      const html = template.html(title, list,
+        `<h2>${title}</h2>${description}`,
+        `
+          <a href="/create">create</a>
+          <a href="/update/${title}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${title}" />
+            <input type="submit" value="delete" />
+          </form>
+        `
+      );
+      res.send(html);
+    });
   });
 });
 
