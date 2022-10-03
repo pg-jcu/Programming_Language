951dd6ff1d3b29367754d9c6baf7db31ee05fb0d
chan uk jeong <40534414+butterstrawberry@users.noreply.github.com>
2022-10-03 22:14:14 +0900

    add

---
+++ react/my_app/components/Nav.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import '../style/Nav.scss';
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <ul>
+        <li>
+          <Link to="/practice">Practice</Link>
+        </li>
+        <li>
+          <Link to="/todo">Todo</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ react/my_app/components/Practice.js
@@ -0,0 +1,37 @@
+import MyComponent from './MyComponent';
+import Counter from './Counter';
+import Say from './Say'
+import Event from './Event';
+import Validation from './Validation';
+import ScrollBox from './ScrollBox';
+import Iteration from './Iteration';
+import Info from './Info';
+import Average from './Average';
+import SassComponent from './SassComponent';
+import { useState } from 'react';
+import '../style/Practice.scss';
+
+function Practice() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => setVisible(!visible);
+
+  return (
+    <div className='Practice'>
+      <MyComponent name='Jeong' number={1}>children</MyComponent>
+      <Counter />
+      <Say />
+      <Event />
+      <Validation />
+      <ScrollBox />
+      <Iteration />
+      <div className='visible'>
+        <button onClick={onClick}>{visible ? 'hidden' : 'visible'}</button>
+        {visible && <Info />}
+      </div>
+      <Average />
+      <SassComponent />
+    </div>
+  );
+}
+
+export default Practice;
