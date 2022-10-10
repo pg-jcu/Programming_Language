31d4cd16ee649fafe0f8071a4ee7db45ad8c9ef9
chan uk jeong <40534414+butterstrawberry@users.noreply.github.com>
2022-10-10 20:28:30 +0900

    add

---
+++ react/my_app/components/WithRouterSample.js
@@ -0,0 +1,29 @@
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import '../style/WithRouterSample.scss';
+
+function WithRouterSample() {
+  const location = useLocation(); 
+  const params = useParams();
+  const navigate = useNavigate();
+
+  return (
+    <div className="WithRouterSample">
+      <hr />
+      <h4>location</h4>
+      <textarea
+        value={JSON.stringify(location, null, 2)}
+        rows={7}
+        readOnly={true}
+      />
+      <h4>params</h4>
+      <textarea
+        value={JSON.stringify(params, null, 2)}
+        rows={7}
+        readOnly={true}
+      />
+      <button onClick={() => navigate('/')}>Home</button>
+    </div>
+  );
+}
+
+export default WithRouterSample;
