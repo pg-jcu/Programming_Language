581eb162ef796cf758a12672ae83e126b50a1ded
chan uk jeong <40534414+butterstrawberry@users.noreply.github.com>
2022-10-12 14:29:42 +0900

    modify

--- react/my_app/components/ColorBox.js
+++ react/my_app/components/ColorBox.js
@@ -1,29 +1,26 @@
-import { ColorConsumer } from '../contexts/color';
+import { useContext } from 'react';
+import ColorContext from '../contexts/color';
 
 function ColorBox() {
+  const { state } = useContext(ColorContext);
+
   return (
-    <div className="ColorBox">
-      <ColorConsumer>
-        {({ state }) => (
-          <>
-            <div
-              style={{
-                width: '64px',
-                height: '64px',
-                background: state.color
-              }}
-            />
-            <div
-              style={{
-                width: '32px',
-                height: '32px',
-                background: state.subcolor
-              }}
-            />
-          </>
-        )}
-      </ColorConsumer>
-    </div>
+    <>
+      <div
+        style={{
+          width: '64px',
+          height: '64px',
+          background: state.color
+        }}
+      />
+      <div
+        style={{
+          width: '32px',
+          height: '32px',
+          background: state.subcolor
+        }}
+      />
+    </>
   );
 }
 
