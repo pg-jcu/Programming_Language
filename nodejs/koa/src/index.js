a0fff2ed7f19f5d1209bbe2f1381349c4b480fe8
jeong <chan3358@naver.com>
2022-10-18 23:20:13 +0900

    modify

--- nodejs/koa/src/index.js
+++ nodejs/koa/src/index.js
@@ -1,22 +1,15 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx) => {
-  ctx.body = 'home';
-});
+router.use('/api', api.routes());
 
-router.get('/about/:name?', (ctx) => {
-  const { name } = ctx.params;
-  ctx.body = name ? `about ${name}` : 'about';
-});
-
-router.get('/posts', (ctx) => {
-  const { id } = ctx.query;
-  ctx.body = id ? `post #${id}` : `No post id`;
-})
+app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
