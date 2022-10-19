23851fe54248f4fe40daf8473d30c0a00dab8f13
jeong <chan3358@naver.com>
2022-10-19 20:22:08 +0900

    add

---
+++ nodejs/study_from_internet/cookie/cookie.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const cookie = require('cookie');
+
+http.createServer((req, res) => {
+  let cookies = {};
+
+  if (req.headers.cookie !== undefined) {
+    cookies = cookie.parse(req.headers.cookie);
+  }
+
+  console.log(req.headers.cookie);
+  console.log(cookies.yummy_cookie);
+
+  res.writeHead(200, {
+    'Set-Cookie': [
+      'yummy_cookie=choco', 
+      'tasty_cookie=strawberry',
+      `Permanent=cookies; Max-Age=${60*60*24*30}`
+    ]
+  });
+  res.end('Cookie');
+}).listen(3000);
