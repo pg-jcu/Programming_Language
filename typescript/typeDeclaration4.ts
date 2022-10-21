3f85de11a3938e30b65f5f9a894b22f909b0be61
jeong <chan3358@naver.com>
2022-10-21 11:14:12 +0900

    add

---
+++ typescript/typeDeclaration4.ts
@@ -0,0 +1,39 @@
+// Void
+function hello(msg: string): void {
+  console.log(`Hello ${msg}`);
+}
+
+// Never
+function error(message: string): never {
+  throw new Error(message);
+}
+
+const never: [] = [];
+// never.push(3); // error
+
+// Intersection
+interface IUser {
+  name: string,
+  age: number
+}
+
+interface IValidation {
+  isValid: boolean
+}
+
+const heropy: IUser & IValidation = {
+  name: 'Heropy',
+  age: 36,
+  isValid: true
+};
+
+// Function
+let myFunc: (arg1: number, arg2: number) => number;
+myFunc = function(x, y) {
+  return x + y;
+}
+
+let yourFunc: () => void;
+yourFunc = () => {
+  console.log('yourFunc');
+}
