20736b73671403f53c0a15b6816e4b5301650095
jeong <chan3358@naver.com>
2022-10-21 11:46:37 +0900

    add

---
+++ typescript/typeInference.ts
@@ -0,0 +1,9 @@
+// Interence
+let numInference = 12; // initialized variable
+// numInference = 'Hello type!'; // error
+
+// parameters with default values
+function add(a: number, b = 2) {
+  // function with return value
+  return a + b;
+}
