d767c4f93a1e3314256e36253a44a2da790885b6
jeong <chan3358@naver.com>
2022-10-23 13:27:32 +0900

    modify

--- nodejs/koa/src/api/posts/index.js
+++ nodejs/koa/src/api/posts/index.js
@@ -1,12 +1,18 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl.js';
+import checkLoggedIn from '../../lib/checkLoggedIn.js';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+posts.post('/', checkLoggedIn, postsCtrl.write);
+
+const post = new Router();
+
+post.get('/', postsCtrl.read);
+post.delete('/', checkLoggedIn, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
