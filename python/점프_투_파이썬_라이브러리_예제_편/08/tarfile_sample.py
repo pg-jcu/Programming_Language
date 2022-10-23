c4da99ade938ffd59ec30f021f975bfc53d682b2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 09:33:53 +0000

    Adding tarfile member sanitization to extractall()

--- python/점프_투_파이썬_라이브러리_예제_편/08/tarfile_sample.py
+++ python/점프_투_파이썬_라이브러리_예제_편/08/tarfile_sample.py
@@ -6,7 +6,29 @@ with tarfile.open("mytext.tar", 'w') as mytar:
     mytar.add("c.txt")
 
 with tarfile.open("mytext.tar") as mytar:
-    mytar.extractall()
+    
+    import os
+    
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(mytar)
 
 with tarfile.open("mytext.tar") as mytar:
     mytar.extract("a.txt")
@@ -17,4 +39,23 @@ with tarfile.open('mytext.tar.gz', 'w:gz') as mytar:
     mytar.add('c.txt')
 
 with tarfile.open('mytext.tar.gz') as mytar:
-    mytar.extractall()
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(mytar)
