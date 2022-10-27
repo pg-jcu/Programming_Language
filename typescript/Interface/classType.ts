8a9fac230d6b2ceb62dc3e7121392dbb66d661fb
jeong <chan3358@naver.com>
2022-10-27 22:25:40 +0900

    add

---
+++ typescript/Interface/classType.ts
@@ -0,0 +1,93 @@
+// Class type
+interface IUserClass {
+  name: string,
+  getName(): string
+}
+
+class User implements IUserClass {
+  constructor(public name: string) {}
+  getName() {
+    return this.name;
+  }
+}
+
+const jeong = new User('Jeong');
+jeong.getName();
+
+// Construct signature
+// interface IName {
+//   new (PARAMETER: PARAM_TYPE): RETURN_TYPE
+// }
+
+interface ICat {
+  name: string
+}
+
+interface ICatConstructor {
+  new (name: string): ICat;
+}
+
+class CatClass implements ICat {
+  constructor(public name: string) {}
+}
+
+function makeKitten(c: ICatConstructor, n: string) {
+  return new c(n);
+}
+
+const kitten = makeKitten(CatClass, 'Lucy');
+
+// example
+interface IFullName {
+  firstName: string,
+  lastName: string,
+}
+interface IFullNameConstructor {
+  new(firstName: string): IFullName; // Construct signature
+}
+
+function makeSon(c: IFullNameConstructor, firstName: string) {
+  return new c(firstName);
+}
+function getFullName(son: IFullName) {
+  return `${son.firstName} ${son.lastName}`;
+}
+
+// Anderson family
+class Anderson implements IFullName {
+  public lastName: string;
+  constructor (public firstName: string) {
+    this.lastName = 'Anderson';
+  }
+}
+const tomas = makeSon(Anderson, 'Tomas');
+const jack = makeSon(Anderson, 'Jack');
+getFullName(tomas); // Tomas Anderson
+getFullName(jack); // Jack Anderson
+
+interface IFullName1 {
+  firstName: string,
+  lastName: string,
+  agentCode?: number
+}
+interface IFullNameConstructor1 {
+  new(firstName: string, agentCode: number): IFullName; // Construct signature
+}
+
+// Smith family
+class Smith implements IFullName1 {
+  public lastName: string;
+  constructor (public firstName: string, public agentCode: number) {
+    this.lastName = `Smith ${agentCode}`;
+  }
+}
+
+function makeSon1(c: IFullNameConstructor1, firstName: string, agentCode: number) {
+  return new c(firstName, agentCode);
+}
+function getFullName1(son: IFullName1) {
+  return `${son.firstName} ${son.lastName}`;
+}
+
+const smith = makeSon1(Smith, 'John', 7);
+getFullName1(smith); // John Smith 7
---
+++ typescript/Interface/indexableTypes.ts
@@ -0,0 +1,37 @@
+// Index signature
+// interface INAME {
+//   [INDEXER_NAME: INDAXER_TYPE]: RETURN_TYPE
+// }
+
+interface IItem {
+  [itemIndex: number]: string | boolean | number[]
+}
+
+let item: IItem = ['Hello', false, [1, 2, 3]];
+
+interface IUserIndex {
+  [userProp: string]: string | boolean
+}
+
+let userIndex: IUserIndex = {
+  name: 'jeong',
+  0: false,
+  // num: 1 // error
+};
+console.log(userIndex[0]);
+console.log(userIndex['0']);
+
+// keyof
+interface ICountries {
+  KR: '대한민국',
+  US: '미국',
+  JP: '일본' 
+}
+
+let country: keyof ICountries; // 'KR' | 'US' | 'JP'
+country = 'KR';
+// country = 'CH'; // error
+
+let country1: ICountries[keyof ICountries]; //  ICountries['KR' | 'US' | 'JP']
+country1 = '대한민국';
+// country1 = '러시아';
