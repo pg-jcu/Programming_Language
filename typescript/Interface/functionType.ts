eaf26d9757ca351465c9a01dc714112a998bcb2c
jeong <chan3358@naver.com>
2022-10-27 13:04:53 +0900

    add

---
+++ typescript/Interface/functionType.ts
@@ -0,0 +1,18 @@
+// Call signature
+// interface IName {
+//   (PARAMETER: PARAM_TYPE): RETURN_TYPE
+// }
+
+interface IUser {
+  name: string
+}
+
+interface IGetUser {
+  (name: string): IUser
+}
+
+const getUser: IGetUser = function(n) { // n is name: string
+  return user;
+};
+
+getUser('Jeong');
---
+++ typescript/Interface/readonlyProperies.ts
@@ -0,0 +1,30 @@
+// Readonly properties
+interface IUserROP {
+  readonly name: string,
+  age: number
+}
+
+let userROP: IUserROP = {
+  name: 'Jeong',
+  age: 28
+};
+
+userROP.age = 10;
+// user1.name = 'chan'; // error
+
+// Readonly Utility
+interface IUserROU {
+  name: string,
+  age: number
+}
+
+let userROU: Readonly<IUserROU> = {
+  name: 'jeong',
+  age: 12
+};
+
+// Type assertion
+let userAss = {
+  name: 'uk',
+  age: 33
+} as const;
