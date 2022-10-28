2b98e7e75c8dd3acee893f936e040132b21c2f05
jeong <chan3358@naver.com>
2022-10-28 23:25:27 +0900

    add

---
+++ nodejs/study_from_internet/lowdb/index.js
@@ -0,0 +1,14 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Low } from 'lowdb';
+import { JSONFile } from 'lowdb/node';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'db.json');
+
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+
+db.data ||= {};
+
+await db.write();
