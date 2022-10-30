fbd60a3d919506c2013dcc4dc6c956d7adfb240f
jeong <chan3358@naver.com>
2022-10-30 16:22:40 +0900

    add

---
+++ react/blog-frontend/components/auth/AuthForm.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const AuthFormBlock = styled.div``;
+
+function AuthForm() {
+  return (
+    <AuthFormBlock>
+      AuthForm
+    </AuthFormBlock>
+  );
+}
+
+export default AuthForm;
---
+++ react/blog-frontend/components/base/Header.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const HeaderBlock = styled.div``;
+
+function Header() {
+  return (
+    <Header>
+
+    </Header>
+  );
+}
+
+export default Header;
