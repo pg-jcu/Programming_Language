2739ff8ccd33797052b2e6a1d21b93df33b3c9e7
jeong <chan3358@naver.com>
2022-10-31 16:34:19 +0900

    modify

--- react/my_app/components/Game/Board.js
+++ react/my_app/components/Game/Board.js
@@ -1,15 +1,44 @@
 import { useState } from "react";
 import Square from "./Square";
 import '../../style/Game/Board.scss';
+import { useMemo } from "react";
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 function Board() {
   const [squares, setSquares] = useState({
     data: Array(9).fill(null),
     xIsNext: true
   });
-  const status = `Next player: ${squares.xIsNext ? 'X' : 'O'}`;
+  const winner = useMemo(() => calculateWinner(squares.data), [squares.data]);
+  const status = winner ? 
+    'Winner: ' + winner : 
+    `Next player: ${squares.xIsNext ? 'X' : 'O'}`;
   const onClick = (i) => {
     const nextData = squares.data.slice();
+
+    if (calculateWinner(nextData) || nextData[i]) {
+      return;
+    }
+
     nextData[i] = squares.xIsNext ? 'X' : 'O';
     setSquares({
       data: nextData,
