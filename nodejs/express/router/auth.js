a2e9b75edf59e98c5d5e326fdcc19f4751d7b65c
jeong <chan3358@naver.com>
2022-11-08 14:17:57 +0900

    add

---
+++ nodejs/express/router/auth.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const template = require('../lib/template.js');
+
+const authData = {
+  email: 'aaa@bbb.ccc',
+  password: '1234',
+  nickname: 'jeong'
+};
+
+router.get('/login', (req, res) => {
+  const title = 'Login';
+  const list = template.list(req.list);
+  const html = template.html(title, list, `
+    <form action="/auth/login" method="post">
+      <p><input type="text" name="email" placeholder="email" /><p>
+      <p><input type="password" name="password" placeholder="password" /><p>
+      <p>
+        <input type="submit" value="login" />
+      </p>
+    </form>
+  `, '');
+  res.send(html);
+});
+
+router.post('/login', (req, res) => {
+  const post = req.body;
+  const email = post.email;
+  const password = post.password;
+
+  if (email === authData.email && password === authData.password) {
+    res.send('Welcome');
+  } else {
+    res.send('Who?');
+  }
+});
+
+module.exports = router;
