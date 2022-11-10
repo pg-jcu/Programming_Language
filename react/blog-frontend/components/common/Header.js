25c821919de8e1f9827e7be6728c016f8297b706
jeong <chan3358@naver.com>
2022-11-10 22:55:53 +0900

    add

---
+++ react/blog-frontend/components/common/Header.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import Responsive from "./Responsive";
+import Button from "./Button";
+import { Link } from "react-router-dom";
+
+const HeaderBlock = styled.div`
+  position: fixed;
+  width: 100%;
+  background: white;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
+`;
+
+const Wrapper = styled(Responsive)`
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .logo {
+    font-size: 1.125rem;
+    font-weight: 800;
+    letter-spacing: 2px;
+  }
+
+  .right {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const Spacer = styled.div`
+  height: 4rem;
+`;
+
+function Header() {
+  return (
+    <>
+      <HeaderBlock>
+        <Wrapper>
+          <Link to="/" className="logo">
+            REACTERS
+          </Link>
+          <div className="right">
+            <Button to="/login">
+              Login
+            </Button>
+          </div>
+        </Wrapper>
+      </HeaderBlock>
+      <Spacer />
+    </>
+  );
+}
+
+export default Header;
