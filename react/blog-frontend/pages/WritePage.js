b665807de4aee3ea4605c7b6d333415f6645457f
jeong <chan3358@naver.com>
2022-11-11 23:10:54 +0900

    modify

--- react/blog-frontend/pages/WritePage.js
+++ react/blog-frontend/pages/WritePage.js
@@ -1,10 +1,14 @@
 import Editor from "../components/write/Editor";
+import TagBox from "../components/write/TagBox";
+import WriteActionButtons from "../components/write/WriteActionButtons";
 import Responsive from "../components/common/Responsive";
 
 function WritePage() {
   return (
     <Responsive>
       <Editor />
+      <TagBox />
+      <WriteActionButtons />
     </Responsive>
   );
 }
