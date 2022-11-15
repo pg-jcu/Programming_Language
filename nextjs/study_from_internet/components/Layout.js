e04c74711511bc119548124bb12e8c79c7a169ef
jeong <chan3358@naver.com>
2022-11-15 15:33:36 +0900

    add

---
+++ nextjs/study_from_internet/components/Layout.js
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import Navbar from "./Navbar";
+import Title from "./Title";
+
+export default function Layout({ children }) {
+  const router = useRouter();
+  const titleList = {'/': 'Home', '/about': 'About'};
+  return (
+    <>
+      <Title title={titleList[router.pathname]} />
+      <Navbar />
+      <div>{children}</div>
+      <style jsx global>{`
+        a:hover {
+          color: orange;
+        }
+      `}</style>
+    </>
+  );
+}
