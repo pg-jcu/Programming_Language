04a2dfb7c0768772621da8d1b178723425461b00
jeong <chan3358@naver.com>
2022-11-16 11:50:49 +0900

    modify

--- nextjs/study_from_internet/components/Navbar.js
+++ nextjs/study_from_internet/components/Navbar.js
@@ -3,23 +3,38 @@ import Link from "next/link";
 export default function Navbar({ pathname }) {
   return (
       <nav>
-        <Link href="/" legacyBehavior>
-          <a className={pathname === "/" ? "active" : ""}>Home</a>
-        </Link>
-        <Link href="/about" legacyBehavior>
-          <a className={pathname === "/about" ? "active" : ""}>About</a>  
-        </Link>
+        <img src="/vercel.svg" alt="image" />
+        <div>
+          <Link href="/" legacyBehavior>
+            <a className={pathname === "/" ? "active" : ""}>Home</a>
+          </Link>
+          <Link href="/about" legacyBehavior>
+            <a className={pathname === "/about" ? "active" : ""}>About</a>  
+          </Link>
+        </div>
         <style jsx>{`
           nav {
-            display: inline-block;
+            display: flex;
+            gap: 10px;
+            flex-direction: column;
+            align-items: center;
+            padding-top: 20px;
+            padding-bottom: 10px;
+            box-shadow: 0px 50px 100px -20px rgba(50, 50, 93, 0.25),
+              0px 30px 60px -30px rgba(0, 0, 0, 0.3);
           }
-
-          a {
-            background: green;
-            color: yellow;
-            margin-right: 5px;
+          nav div {
+            display: flex;
+            gap: 10px;
           }
-
+          nav a {
+            font-weight: 600;
+            font-size: 18px;
+          }
+          img {
+            max-width: 100px;
+            margin-bottom: 5px;
+          }
           .active {
             color: red;
           }
