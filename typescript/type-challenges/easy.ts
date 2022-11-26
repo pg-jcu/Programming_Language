c1fb7d54c1cc7da3c41b7a831373a8c344b631cf
jeong <chan3358@naver.com>
2022-11-26 20:45:34 +0900

    add

---
+++ typescript/type-challenges/easy.ts
@@ -0,0 +1,27 @@
+// 4 - Pick
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+};
+
+// 7 - Readonly
+type MyReadonly<T> = {
+  readonly [K in keyof T]: T[K]
+};
+
+// 11 - Tuple to Object
+type TupleToObject<T extends readonly (string | number)[]> = {
+  [K in T[number]]: K
+};
+
+// 14 - First of Array
+type First<T extends any[]> = T extends [] ? never : T[0];
+
+// 18 - Length of Tuple
+type Length<T extends readonly any[]> = T['length'];
+
+// 43 - Exclude
+type MyExclude<T, U> = T extends U ? never : T;
+
+// 189 - Awaited
+type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U> 
+  ? U extends PromiseLike<any> ? MyAwaited<U> : U : never;
