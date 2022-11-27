2970bd0cad54b0b1c7e475cfd0dce34cfbbfc2ad
jeong <chan3358@naver.com>
2022-11-27 10:30:15 +0900

    add

--- typescript/type-challenges/easy.ts
+++ typescript/type-challenges/easy.ts
@@ -25,3 +25,20 @@ type MyExclude<T, U> = T extends U ? never : T;
 // 189 - Awaited
 type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U> 
   ? U extends PromiseLike<any> ? MyAwaited<U> : U : never;
+
+// 268 - If
+type If<C extends boolean, T, F> = C extends true ? T : F;
+
+// 533 - Concat
+type Concat<T extends Array<any>, U extends Array<any>> = [...T, ...U];
+
+// 898 - Includes
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false;
+
+type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest]
+  ? Equal<First, U> extends true ? true : Includes<Rest, U>
+  : false;
