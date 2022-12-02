34c92313021f46403a06ed2bf7579d5c37207dae
jeong <chan3358@naver.com>
2022-12-02 14:25:59 +0900

    add

--- typescript/type-challenges/medium.ts
+++ typescript/type-challenges/medium.ts
@@ -1,2 +1,14 @@
 // 2 - Get Return Type
 type MyReturnType<T extends Function> = T extends (...args: any[]) => infer R ? R : never;
+
+// 3 - Omit
+type MyOmit<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P]
+};
+
+// 8 - Readonly 2
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  [P in keyof T as (P extends K ? never : P)]: T[P]
+} & {
+  readonly [P in K]: T[P]
+};
