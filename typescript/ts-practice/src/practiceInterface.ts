302213b7c985a66b5a02a25ff370f9fab65306f6
jeong <chan3358@naver.com>
2022-12-04 22:24:28 +0900

    modify

--- typescript/ts-practice/src/practiceInterface.ts
+++ typescript/ts-practice/src/practiceInterface.ts
@@ -3,9 +3,9 @@ interface Shape {
 }
 
 class Circle implements Shape {
-  radius: number;
+  // radius: number;
   
-  constructor(radius: number) {
+  constructor(public radius: number) {
     this.radius = radius;
   }
 
@@ -15,10 +15,10 @@ class Circle implements Shape {
 }
 
 class Rectangle implements Shape {
-  width: number;
-  height: number;
+  // width: number;
+  // height: number;
 
-  constructor(width: number, height: number) {
+  constructor(private width: number, private height: number) {
     this.width = width;
     this.height = height;
   }
@@ -28,8 +28,35 @@ class Rectangle implements Shape {
   }
 }
 
+const circle = new Circle(5);
+const rectangle = new Rectangle(10, 5);
+
+console.log(circle.radius);
+// console.log(rectangle.width); // ts2341
+
 const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
 
 shapes.forEach(shape => {
   console.log(shape.getArea());
 });
+
+interface Person {
+  name: string;
+  age?: number;
+}
+
+interface Developer extends Person {
+  skills: string[];
+}
+
+const person: Person = {
+  name: 'jeong',
+  age: 28,
+};
+
+const expert: Developer = {
+  name: 'jeong',
+  skills: ['javascript', 'react']
+};
+
+const people: Person[] = [person, expert];
