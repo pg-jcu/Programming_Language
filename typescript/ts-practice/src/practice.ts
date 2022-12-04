9479fbb871a5d9ff67fa4e8574506ee45a92b4a8
jeong <chan3358@naver.com>
2022-12-04 15:56:05 +0900

    modify

--- typescript/ts-practice/src/practice.ts
+++ typescript/ts-practice/src/practice.ts
@@ -1,2 +1,34 @@
+// Type
+let count = 0;
+count += 1;
+// count = 'text'; // ts2322
+
 const message: string = 'hello world';
-console.log(message);
+
+const done: boolean = true;
+
+const numbers: number[] = [1, 2, 3];
+const messages: string[] = ['hello', 'world'];
+
+// messages.push(1); // ts2345
+
+let mightBeUndefined: string | undefined = undefined;
+let nullableNumber: number | null = null;
+
+let color: 'red' | 'orange' | 'yellow' = 'red';
+color = 'yellow';
+// color = 'green'; // ts2322
+
+
+// Function
+function sum(x: number, y: number): number {
+  return x + y;
+}
+
+function sumArray(numbers: number[]): number {
+  return numbers.reduce((acc, cur) => acc + cur, 0);
+}
+
+function returnNothing(): void {
+  console.log('return nothing...');
+}
