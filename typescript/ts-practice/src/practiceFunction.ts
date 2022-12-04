fbcce1d2cd5857f18cbde5d7a672db23afb053c8
jeong <chan3358@naver.com>
2022-12-04 16:02:25 +0900

    split file

---
+++ typescript/ts-practice/src/practiceFunction.ts
@@ -0,0 +1,16 @@
+function sum(x: number, y: number): number {
+  return x + y;
+}
+
+function sub(x: number, y: number): number {
+  // return null; // ts2322
+  return x - y;
+}
+
+function sumArray(numbers: number[]): number {
+  return numbers.reduce((acc, cur) => acc + cur, 0);
+}
+
+function returnNothing(): void {
+  console.log('return nothing...');
+}
--- typescript/ts-practice/src/practiceType.ts
+++ typescript/ts-practice/src/practiceType.ts
@@ -18,17 +18,3 @@ let nullableNumber: number | null = null;
 let color: 'red' | 'orange' | 'yellow' = 'red';
 color = 'yellow';
 // color = 'green'; // ts2322
-
-
-// Function
-function sum(x: number, y: number): number {
-  return x + y;
-}
-
-function sumArray(numbers: number[]): number {
-  return numbers.reduce((acc, cur) => acc + cur, 0);
-}
-
-function returnNothing(): void {
-  console.log('return nothing...');
-}
