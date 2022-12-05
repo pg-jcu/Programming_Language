06686d7c52c61b0813d4d6fd17e7e6c20ed1e9b7
jeong <chan3358@naver.com>
2022-12-05 19:53:13 +0900

    add

---
+++ react/study_from_internet/ts-tutorial/src/Counter.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./styles/Counter.css";
+
+function Counter() {
+  const [count, setCount] = useState<number>(0);
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
+
+  return (
+    <div className="Counter">
+      <h2>{count}</h2>
+      <div>
+        <button onClick={onIncrease}>+1</button>
+        <button onClick={onDecrease}>-1</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
