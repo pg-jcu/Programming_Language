be46901b66aa79d4378fee49e083822e73a14df1
jeong <chan3358@naver.com>
2022-12-05 16:08:02 +0900

    add

---
+++ react/study_from_internet/ts-tutorial/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Greetings from './Greetings';
+
+function App() {
+  return (
+    <div className="App">
+      <Greetings name="jeong" />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/study_from_internet/ts-tutorial/src/Greetings.tsx
@@ -0,0 +1,18 @@
+interface GreetingsProps {
+  name: string;
+  mark: string;
+}
+
+function Greetings({ name, mark }: GreetingsProps) {
+  return (
+    <div>
+      Hello, {name} {mark}
+    </div>
+  );
+}
+
+Greetings.defaultProps = {
+  mark: '!',
+};
+
+export default Greetings;
