f4a851d06f42a01c13c40e49ae6a9b73edc65a0c
jeong <chan3358@naver.com>
2022-12-05 21:42:08 +0900

    add

---
+++ react/study_from_internet/ts-tutorial/src/MyForm.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./styles/MyForm.css";
+
+interface Form {
+  name: string;
+  description: string;
+}
+
+function MyForm() {
+  const [form, setForm] = useState<Form>({
+    name: '',
+    description: '',
+  });
+  const { name, description } = form;
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
+    setForm({
+      name: '',
+      description: '',
+    });
+  };
+
+  return (
+    <div className="MyForm">
+      <form onSubmit={onSubmit}>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="description" value={description} onChange={onChange} />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default MyForm;
