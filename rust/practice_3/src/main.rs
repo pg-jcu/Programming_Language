7339d8c37e030ed000d070f45902e534341b20e9
jeong <chan3358@naver.com>
2022-12-20 13:48:26 +0900

    add

--- rust/practice_3/src/main.rs
+++ rust/practice_3/src/main.rs
@@ -1,11 +1,22 @@
 fn main() {
+    println!("Practice 3.1");
     practice3_1();
+    println!("---------------------------");
+
+    println!("Practice 3.2");
     practice3_2();
+    println!("---------------------------");
+
+    println!("Practice 3.3");
+    println!("practice3_3 return: {}", practice3_3(3));
+    println!("---------------------------");
+
+    println!("Practice 3.5");
+    practice3_5(5);
+    println!("---------------------------");
 }
 
 fn practice3_1() {
-    println!("Practice 3.1");
-
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
@@ -18,13 +29,9 @@ fn practice3_1() {
     let y = y + 1;
     let y = y * 2;
     println!("The value of y is: {}", y);
-
-    println!("---------------------------");
 }
 
 fn practice3_2() {
-    println!("Practice 3.2");
-
     let x = 2.0;
     let y: f32 = 3.0;
     let f: bool = false;
@@ -38,6 +45,51 @@ fn practice3_2() {
 
     let a = [1, 2, 3];
     println!("a[0] a[1] a[2]: {} {} {}", a[0], a[1], a[2]);
+}
 
-    println!("---------------------------");
+fn practice3_3(x: i32) -> i32 {
+    println!("Param x: {}", x);
+
+    let y = {
+        let z = 3;
+        z + 1
+    };
+
+    println!("The value of y is: {}", y);
+
+    5
+}
+
+fn practice3_5(number: i32) {
+    if number > 0 {
+        println!("number > 0");
+    } else if number < 0 {
+        println!("number < 0");
+    } else {
+        println!("number == 0");
+    }
+
+    let even_or_odd = if number % 2 == 0 {
+        "even"
+    } else {
+        "odd"
+    };
+
+    println!("number is {}", even_or_odd);
+
+    loop {
+        println!("loop");
+        break;
+    }
+
+    let mut tmp = number;
+    while tmp < 10 {
+        println!("while number count: {}", tmp);
+
+        tmp += 1;
+    }
+
+    for num in (1..4).rev() {
+        println!("for range reverse: {}", num);
+    }
 }
