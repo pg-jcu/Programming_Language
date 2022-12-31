11a37908391fd8f6b28bcb6c5e9b1b1fe8ff81f8
jeong <chan3358@naver.com>
2022-12-31 11:21:07 +0900

    add

---
+++ rust/practice_10/src/lib.rs
@@ -0,0 +1,37 @@
+pub trait Summarizable {
+    fn author_summary(&self) -> String;
+
+    fn summary(&self) -> String {
+        format!("(Read more from {}...)", self.author_summary())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summarizable for NewsArticle {
+    fn author_summary(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String, 
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn author_summary(&self) -> String {
+        format!("@{}", self.username)
+    }
+
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
