f885b3c1ea93477d7580bbcdf3aaa7202cb729ed
jeong <chan3358@naver.com>
2023-01-05 13:59:20 +0900

    add

---
+++ typescript/ts-tips/src/tip_4.ts
@@ -0,0 +1,24 @@
+// Use type predicate to avoid type assertion
+type Circle = { kind: 'circle'; radius: number; };
+type Rect = { kind: 'rect'; width: number; height: number; };
+type Shape = Circle | Rect;
+
+function isCircle(shape: Shape) {
+  return shape.kind === 'circle';
+}
+
+function isRect(shape: Shape): shape is Rect {
+  return shape.kind === 'rect';
+}
+
+function getShapes() {
+  return [];
+}
+
+const myShapes: Shape[] = getShapes();
+
+// error because typescript doesn't know the filtering
+// const circles: Circle[] = myShapes.filter(isCircle);
+
+// now you get Rect[] type inferred correctly
+const rects = myShapes.filter(isRect);
