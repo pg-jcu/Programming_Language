9c785a61d94f185c4f46deef2aa75b0e3beb631d
jeong <chan3358@naver.com>
2023-01-09 16:31:06 +0900

    add

--- rust/practice_13/src/main.rs
+++ rust/practice_13/src/main.rs
@@ -105,4 +105,66 @@ fn practice_13_2() {
     let v4: Vec<_> = v3.iter().map(|x| x + 1).collect();
 
     assert_eq!(v4, vec![2, 3, 4]);
+
+    #[derive(Debug, PartialEq)]
+    struct Shoe {
+        size: u32,
+        style: String,
+    }
+
+    fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+        shoes.into_iter()
+            .filter(|s| s.size == shoe_size)
+            .collect()
+    }
+
+    let shoes = vec![
+        Shoe { size: 10, style: String::from("sneaker") },
+        Shoe { size: 13, style: String::from("sandal") },
+        Shoe { size: 10, style: String::from("boot") },
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+
+    println!("In my size: {:?}", in_my_size);
+
+    struct Counter {
+        count: u32,
+    }
+
+    impl Counter {
+        fn new() -> Counter {
+            Counter { count: 0 }
+        }
+    }
+
+    impl Iterator for Counter {
+        type Item = u32;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.count += 1;
+
+            if self.count < 6 {
+                Some(self.count)
+            } else {
+                None
+            }
+        }
+    }
+
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+
+    let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+                                .map(|(a, b)| a * b)
+                                .filter(|x| x % 3 == 0)
+                                .sum();
+    
+    println!("Counter sum: {}", sum);
 }
