73c868df143a7a8a6e6452425dfa2135d345684e
jeong <chan3358@naver.com>
2023-01-13 15:30:37 +0900

    add

--- rust/book/practice_15/src/main.rs
+++ rust/book/practice_15/src/main.rs
@@ -10,6 +10,10 @@ fn main() {
     println!("Practice 15.3");
     practice_15_3();
     println!("---------------------------");
+
+    println!("Practice 15.4");
+    practice_15_4();
+    println!("---------------------------");
 }
 
 fn practice_15_1() {
@@ -83,4 +87,24 @@ fn practice_15_3() {
     let c = CustomSmartPointer { data: String::from("C") };
     println!("CustomSmartPointers created.");
     drop(a);
+}
+
+fn practice_15_4() {
+    enum List {
+        Cons(i32, Rc<List>),
+        Nil,
+    }
+
+    use List::{Cons, Nil};
+    use std::rc::Rc;
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
