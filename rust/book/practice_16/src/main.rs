467e0886b72c7766d8b1ee751ed681df7a7c7caa
jeong <chan3358@naver.com>
2023-01-21 21:42:12 +0900

    add

---
+++ rust/book/practice_16/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("Practice 16.1");
+    practice_16_1();
+    println!("---------------------------");
+}
+
+fn practice_16_1() {
+    use std::thread;
+    use std::time::Duration;
+
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("The spawned thread: {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("The main thread: {}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
