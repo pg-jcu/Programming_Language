7f76411e8d0dc663f9964108657b2b421fd3f3d8
jeong <chan3358@naver.com>
2023-01-25 16:39:36 +0900

    add

--- rust/book/practice_16/src/main.rs
+++ rust/book/practice_16/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     println!("Practice 16.1");
     practice_16_1();
     println!("---------------------------");
+
+    println!("Practice 16.2");
+    practice_16_2();
+    println!("---------------------------");
 }
 
 fn practice_16_1() {
@@ -29,4 +33,55 @@ fn practice_16_1() {
     });
 
     handle_v.join().unwrap();
+}
+
+fn practice_16_2() {
+    use std::thread;
+    use std::sync::mpsc;
+    use std::time::Duration;
+
+    let (tx1, rx1) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx1.send(val).unwrap();
+    });
+
+    let received = rx1.recv().unwrap();
+    println!("Got: {}", received);
+
+    let (tx2, rx2) = mpsc::channel();
+
+    let tx3 = mpsc::Sender::clone(&tx2);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hello"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx3.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx2 {
+        println!("Got: {}", received);
+    }
 }
