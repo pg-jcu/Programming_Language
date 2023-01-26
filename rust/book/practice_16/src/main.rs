a4ce5ef980dccf78bc6fec4dccba9e6c560e7b5f
jeong <chan3358@naver.com>
2023-01-26 21:15:27 +0900

    add

--- rust/book/practice_16/src/main.rs
+++ rust/book/practice_16/src/main.rs
@@ -6,6 +6,10 @@ fn main() {
     println!("Practice 16.2");
     practice_16_2();
     println!("---------------------------");
+
+    println!("Practice 16.3");
+    practice_16_3();
+    println!("---------------------------");
 }
 
 fn practice_16_1() {
@@ -84,4 +88,37 @@ fn practice_16_2() {
     for received in rx2 {
         println!("Got: {}", received);
     }
+}
+
+fn practice_16_3() {
+    use std::sync::{Mutex, Arc};
+    use std::thread;
+
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
