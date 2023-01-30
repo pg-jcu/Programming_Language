9768f4e66069f19daed418839ed9bbcf8edb7691
jeong <chan3358@naver.com>
2023-01-30 21:51:43 +0900

    modify

--- rust/rust-by-example/variable_bindings/src/main.rs
+++ rust/rust-by-example/variable_bindings/src/main.rs
@@ -1,7 +1,19 @@
 fn main() {
+    println!("variable bindings");
     variable_bindings();
+    println!("-------------------------");
+
+    println!("scope and shadowing");
+    scope_and_shadowing();
+    println!("-------------------------");
+
+    println!("declare first");
     declare_first();
+    println!("-------------------------");
+
+    println!("freezing");
     freezing();
+    println!("-------------------------");
 }
 
 fn variable_bindings() {
@@ -17,6 +29,24 @@ fn variable_bindings() {
     // The compiler warns about unused variable bindings.
     // This warning can be silenced by prefixing the variable name with an underscore.
     let _unused_variable = 3u32;
+}
+
+fn scope_and_shadowing() {
+    let shadowed_binding = 1;
+
+    {
+        println!("before being shadowed: {}", shadowed_binding);
+
+        let shadowed_binding = "abc";
+
+        println!("shadowed in inner block: {}", shadowed_binding);
+    }
+
+    println!("outside inner block: {}", shadowed_binding);
+
+    let shadowed_binding = 2;
+    
+    println!("shadowed in outer block: {}", shadowed_binding);
 }
 
 fn declare_first() {
