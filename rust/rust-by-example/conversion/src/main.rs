f2b4cc91856343de79e1e08954436131c0afd5bf
jeong <chan3358@naver.com>
2023-02-08 21:26:52 +0900

    add

--- rust/rust-by-example/conversion/src/main.rs
+++ rust/rust-by-example/conversion/src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     println!("From and Into");
     from_and_into();
     println!("-------------------------");
+
+    println!("TryFrom and TryInto");
+    tryfrom_and_tryinto();
+    println!("-------------------------");
 }
 
 fn from_and_into() {
@@ -27,4 +31,35 @@ fn from_and_into() {
     let int = 5;
     let num: Number = int.into();
     println!("My number is {:?}", num);
+}
+
+fn tryfrom_and_tryinto() {
+    #[derive(Debug, PartialEq)]
+    struct EvenNumber(i32);
+
+    impl TryFrom<i32> for EvenNumber {
+        type Error = ();
+
+        fn try_from(value: i32) -> Result<Self, Self::Error> {
+            if value % 2 == 0 {
+                Ok(EvenNumber(value))
+            } else {
+                Err(())
+            }
+        }
+    }
+
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+
+    println!("EvenNumber::try_from(8) -> {:?}", EvenNumber::try_from(8));
+    println!("EvenNumber::try_from(5) -> {:?}", EvenNumber::try_from(5));
+
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    println!("try_into: {:?}", result);
+
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
+    println!("try_into: {:?}", result);
 }
