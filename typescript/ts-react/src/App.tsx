5656862e6730c274fc2ed6ca88d6525113118d34
jeong <chan3358@naver.com>
2023-02-08 22:42:16 +0900

    add

---
+++ typescript/ts-react/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://seeyoujeong.github.io" target="_blank">
+          <img src="../public/smile.png" className="logo smile" alt="My logo" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default App
