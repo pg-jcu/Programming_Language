16fca54d39be7abbb58c3066878e09ef2d1a9662
jeong <chan3358@naver.com>
2023-02-10 22:18:00 +0900

    add

--- rust/book/practice_19/src/main.rs
+++ rust/book/practice_19/src/main.rs
@@ -171,4 +171,58 @@ fn practice_19_3() {
     }
 
     println!("Millimeters(10) + Meters(1) = {:?}", Millimeters(10) + Meters(1));
+
+    trait Pilot {
+        fn fly(&self);
+    }
+
+    trait Wizard {
+        fn fly(&self);
+    }
+
+    struct Human;
+
+    impl Pilot for Human {
+        fn fly(&self) {
+            println!("This is your caption speaking.");
+        }
+    }
+
+    impl Wizard for Human {
+        fn fly(&self) {
+            println!("Up!");
+        }
+    }
+
+    impl Human {
+        fn fly(&self) {
+            println!("*waving arms furiously*");
+        }
+    }
+
+    let person = Human;
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    person.fly();
+
+    trait Animal {
+        fn baby_name() -> String;
+    }
+
+    struct Dog;
+
+    impl Dog {
+        fn baby_name() -> String {
+            String::from("Spot")
+        }
+    }
+
+    impl Animal for Dog {
+        fn baby_name() -> String {
+            String::from("puppy")
+        }
+    }
+
+    println!("The baby dog is called {}.", Dog::baby_name());
+    println!("A baby dog is called a {}.", <Dog as Animal>::baby_name());
 }
