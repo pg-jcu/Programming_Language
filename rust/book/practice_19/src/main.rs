905780602524e60ee68efe4e0f4235fba804876a
jeong <chan3358@naver.com>
2023-02-10 23:05:12 +0900

    add

--- rust/book/practice_19/src/main.rs
+++ rust/book/practice_19/src/main.rs
@@ -225,4 +225,40 @@ fn practice_19_3() {
 
     println!("The baby dog is called {}.", Dog::baby_name());
     println!("A baby dog is called a {}.", <Dog as Animal>::baby_name());
+
+    use std::fmt;
+
+    trait OutlinePrint: fmt::Display {
+        fn outline_print(&self) {
+            let output = self.to_string();
+            let len = output.len();
+            println!("{}", "*".repeat(len + 4));
+            println!("*{}*", " ".repeat(len + 2));
+            println!("* {} *", output);
+            println!("*{}*", " ".repeat(len + 2));
+            println!("{}", "*".repeat(len + 4));
+        }
+    }
+
+    impl fmt::Display for Point {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "({}, {})", self.x, self.y)
+        }
+    }
+
+    impl OutlinePrint for Point {}
+
+    let point = Point { x: 1, y: 1 };
+    point.outline_print();
+
+    struct Wrapper(Vec<String>);
+
+    impl fmt::Display for Wrapper {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "[{}]", self.0.join(", "))
+        }
+    }
+
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
 }
