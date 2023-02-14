663808b30978b9b0364351f31248c7105bc3a2e5
jeong <chan3358@naver.com>
2023-02-14 17:33:25 +0900

    add

--- rust/rust-by-example/flow_of_control/src/main.rs
+++ rust/rust-by-example/flow_of_control/src/main.rs
@@ -2,6 +2,14 @@ fn main() {
     println!("if/else");
     if_else();
     println!("-------------------------");
+
+    println!("loop");
+    loop_keyword();
+    println!("-------------------------");
+
+    println!("while");
+    while_keyword();
+    println!("-------------------------");
 }
 
 fn if_else() {
@@ -25,4 +33,77 @@ fn if_else() {
         };
 
     println!("{} -> {}", n, big_n);
+}
+
+fn loop_keyword() {
+    let mut count = 0u32;
+
+    println!("Let's count until infinity!");
+
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+
+            break;
+        }
+    }
+
+    println!("nesting and labels");
+
+    #[allow(unused_labels, unreachable_code)]
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
+
+    println!("returning from loops");
+
+    let mut count = 0;
+
+    let result = loop {
+        count += 1;
+
+        if count == 10 {
+            break count * 2;
+        }
+    };
+
+    println!("result: {}", result);
+}
+
+fn while_keyword() {
+    let mut n = 1;
+
+    while n < 101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+
+        n += 1;
+    }
 }
