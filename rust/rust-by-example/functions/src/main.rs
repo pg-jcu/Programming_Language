6236e810ce63a96e4f4d5139ead617af1a5b1296
jeong <chan3358@naver.com>
2023-02-20 22:19:41 +0900

    add

--- rust/rust-by-example/functions/src/main.rs
+++ rust/rust-by-example/functions/src/main.rs
@@ -190,4 +190,41 @@ fn closures() {
 
     println!("haystack contains 1? {}", contains(&1));
     println!("haystack contains 4? {}", contains(&4));
+
+    println!("as input parameters");
+
+    fn apply<F>(f: F) 
+        where F: FnOnce() {
+        f();
+    }
+
+    fn apply_to_3<F>(f: F) -> i32 
+        where F: Fn(i32) -> i32 {
+        f(3)
+    }
+
+    let greeting = "hello";
+
+    // 'to_owned' creates owned data from borrowed one
+    let mut farewell = "goodbye".to_owned();
+
+    // capture 2 variables
+    let diary = || {
+        // requires 'Fn'
+        println!("I said {}.", greeting);
+
+        // requires 'FnMut'
+        farewell.push_str("!!!");
+        println!("Then I screamed {}.", farewell);
+        println!("Now I can sleep. zzzzz");
+
+        // requires 'FnMut'
+        mem::drop(farewell);
+    };
+
+    apply(diary);
+
+    let double = |x| 2 * x;
+
+    println!("3 doubled: {}", apply_to_3(double));
 }
