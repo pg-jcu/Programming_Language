ffdfd80a7a4ab598d46b59360370f36c48b4413b
jeong <chan3358@naver.com>
2023-02-22 17:21:29 +0900

    add

--- rust/rust-by-example/functions/src/main.rs
+++ rust/rust-by-example/functions/src/main.rs
@@ -242,4 +242,32 @@ fn closures() {
 
     call_me(closure);
     call_me(function);
+
+    println!("as output parameters");
+
+    fn create_fn() -> impl Fn() {
+        let text = "Fn".to_owned();
+
+        move || println!("This is a: {}", text)
+    }
+
+    fn create_fnmut() -> impl FnMut() {
+        let text = "FnMut".to_owned();
+
+        move || println!("This is a: {}", text)
+    }
+
+    fn create_fnonce() -> impl FnOnce() {
+        let text = "FnOnce".to_owned();
+
+        move || println!("This is a: {}", text)
+    }
+
+    let fn_plain = create_fn();
+    let mut fn_mut = create_fnmut();
+    let fn_once = create_fnonce();
+
+    fn_plain();
+    fn_mut();
+    fn_once();
 }
