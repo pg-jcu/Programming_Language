2e95ed2571bee9161312bee9cbbd8f6b19344f8b
jeong <chan3358@naver.com>
2023-02-24 15:55:07 +0900

    add

--- rust/rust-by-example/functions/src/main.rs
+++ rust/rust-by-example/functions/src/main.rs
@@ -271,6 +271,8 @@ fn closures() {
     fn_mut();
     fn_once();
 
+    println!("Iterator::any");
+
     // trait Iterator {
     //     type Item;
 
@@ -297,4 +299,37 @@ fn closures() {
 
     println!("2 in array1: {}", array1.iter().any(|&x| x == 2));
     println!("2 in array2: {}", array2.into_iter().any(|x| x == 2));
+
+    println!("searching through iterators");
+
+    // trait Iterator {
+    //     type Item;
+
+    //     fn find<P>(&mut self, predicate: P) -> Option<Self::Item>
+    //         where P: FnMut(&Self::Item) -> boo;
+    // }
+
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    let mut iter = vec1.iter();
+    let mut into_iter = vec2.into_iter();
+
+    println!("Find 2 in vec1: {:?}", iter.find(|&&x| x == 2));
+    println!("Find 2 in vec2: {:?}", into_iter.find(|&x| x == 2));
+
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+
+    println!("Find 2 in array1: {:?}", array1.iter().find(|&&x| x == 2));
+    println!("Find 2 in array2: {:?}", array2.into_iter().find(|&x| x == 2));
+
+    let vec = vec![1, 9, 3, 3, 13, 2];
+
+    // 'position()' does not take a reference.
+    let index_of_first_even_number = vec.iter().position(|&x| x % 2 == 0);
+    assert_eq!(index_of_first_even_number, Some(5));
+
+    let index_of_first_negative_number = vec.into_iter().position(|x| x < 0);
+    assert_eq!(index_of_first_negative_number, None);
 }
