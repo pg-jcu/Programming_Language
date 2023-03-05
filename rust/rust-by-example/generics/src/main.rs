5d60ea34e825bd009ca069a7197ca7365bc42c44
jeong <chan3358@naver.com>
2023-03-05 22:18:27 +0900

    add

--- rust/rust-by-example/generics/src/main.rs
+++ rust/rust-by-example/generics/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     println!("generics");
     generics();
     println!("-------------------------");
+
+    println!("functions");
+    functions();
+    println!("-------------------------");
 }
 
 fn generics() {
@@ -18,4 +22,28 @@ fn generics() {
     let _t = SingleGen(A);
     let _i32 = SingleGen(6);
     let _char = SingleGen('a');
+}
+
+fn functions() {
+    struct A;
+    struct S(A);
+    struct SGen<T>(T);
+
+    // this functions are not generic
+    fn reg_fn(_s: S) {}
+    fn gen_spec_t(_s: SGen<A>) {}
+    fn gen_spec_i32(_s: SGen<i32>) {}
+
+    // this function is generic
+    fn generic<T>(_s: SGen<T>) {}
+
+    reg_fn(S(A));
+    gen_spec_t(SGen(A));
+    gen_spec_i32(SGen(6));
+
+    // explicitly specified
+    generic::<char>(SGen('a'));
+
+    // implicitly specified
+    generic(SGen('c'));
 }
