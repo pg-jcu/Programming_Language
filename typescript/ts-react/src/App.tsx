03e139d11f2a475392f194018f44b2056da40a48
jeong <chan3358@naver.com>
2023-03-06 22:54:12 +0900

    add

--- typescript/ts-react/src/App.tsx
+++ typescript/ts-react/src/App.tsx
@@ -1,4 +1,5 @@
-import './App.css'
+import { Link, Outlet } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
@@ -8,8 +9,12 @@ function App() {
           <img src="../public/smile.png" className="logo smile" alt="My logo" />
         </a>
       </div>
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/about">About</Link>
+      <Outlet />
     </div>
-  )
+  );
 }
 
 export default App
--- typescript/ts-react/src/main.tsx
+++ typescript/ts-react/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import About from './About';
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'about',
+        element: <About />,
+      }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
