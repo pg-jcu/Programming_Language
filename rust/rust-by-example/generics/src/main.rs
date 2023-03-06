d67cd0e876a3d6307ddab58e565f986c05b36e14
jeong <chan3358@naver.com>
2023-03-06 16:23:10 +0900

    add

--- rust/rust-by-example/generics/src/main.rs
+++ rust/rust-by-example/generics/src/main.rs
@@ -6,6 +6,10 @@ fn main() {
     println!("functions");
     functions();
     println!("-------------------------");
+
+    println!("implementation");
+    implementation();
+    println!("-------------------------");
 }
 
 fn generics() {
@@ -46,4 +50,31 @@ fn functions() {
 
     // implicitly specified
     generic(SGen('c'));
+}
+
+fn implementation() {
+    struct Val {
+        val: f64,
+    }
+
+    struct GenVal<T> {
+        gen_val: T,
+    }
+
+    impl Val {
+        fn value(&self) -> &f64 {
+            &self.val
+        }
+    }
+
+    impl<T> GenVal<T> {
+        fn value(&self) -> &T {
+            &self.gen_val
+        }
+    }
+
+    let x = Val { val: 3.0 };
+    let y = GenVal { gen_val: 3i32 };
+
+    println!("{}, {}", x.value(), y.value());
 }
