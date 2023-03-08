bd8ee3caa9f87225417f2f8b3ab92f231af5b7fa
jeong <chan3358@naver.com>
2023-03-08 22:40:47 +0900

    modify

--- rust/rust-by-example/generics/src/main.rs
+++ rust/rust-by-example/generics/src/main.rs
@@ -141,12 +141,12 @@ fn bounds() {
     impl Red for Cardinal {}
     impl Blue for BlueJay {}
 
-    fn red<T: Red>(_: &T)   -> &'static str { "red" }
+    fn red<T: Red>(_: &T) -> &'static str { "red" }
     fn blue<T: Blue>(_: &T) -> &'static str { "blue" }
 
     let cardinal = Cardinal;
     let blue_jay = BlueJay;
-    let _turkey   = Turkey;
+    let _turkey = Turkey;
 
     println!("A cardinal is {}", red(&cardinal));
     println!("A blue jay is {}", blue(&blue_jay));
