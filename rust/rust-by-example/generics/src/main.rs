f4f6ebce6dfd669a595c7dbb8469cd4ad9fdc00e
jeong <chan3358@naver.com>
2023-03-10 22:34:09 +0900

    add

--- rust/rust-by-example/generics/src/main.rs
+++ rust/rust-by-example/generics/src/main.rs
@@ -18,6 +18,10 @@ fn main() {
     println!("bounds");
     bounds();
     println!("-------------------------");
+
+    println!("multiple bounds");
+    multiple_bounds();
+    println!("-------------------------");
 }
 
 fn generics() {
@@ -150,4 +154,26 @@ fn bounds() {
 
     println!("A cardinal is {}", red(&cardinal));
     println!("A blue jay is {}", blue(&blue_jay));
+}
+
+fn multiple_bounds() {
+    use std::fmt::{Debug, Display};
+
+    fn compare_prints<T: Debug + Display>(t: &T) {
+        println!("Debug: '{:?}'", t);
+        println!("Display: '{}'", t);
+    }
+
+    fn compare_types<T: Debug, U: Debug>(t: &T, u: &U) {
+        println!("t: '{:?}'", t);
+        println!("u: '{:?}'", u);
+    }
+
+    let string = "words";
+    let array = [1, 2, 3];
+    let vec = vec![1, 2, 3];
+
+    compare_prints(&string);
+
+    compare_types(&array, &vec);
 }
