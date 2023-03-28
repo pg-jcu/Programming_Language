a3ad495d80bc39fd7f659054b95fefab75bd4343
jeong <chan3358@naver.com>
2023-03-28 20:20:11 +0900

    add

--- rust/rust-by-example/scoping_rules/src/main.rs
+++ rust/rust-by-example/scoping_rules/src/main.rs
@@ -151,4 +151,43 @@ fn borrowing() {
     borrow_book(&mutabook);
 
     new_edition(&mut mutabook);
+
+    println!("aliasing");
+
+    struct Point { x: i32, y: i32, z: i32 }
+
+    let mut point = Point { x: 0, y: 0, z: 0 };
+
+    let borrowed_point = &point;
+    let another_borrow = &point;
+
+    println!("Point has coordinates: ({}, {}, {})", 
+             borrowed_point.x, another_borrow.y, point.z);
+
+    // error! can't borrow 'point' as mutable because it's currently
+    // borrowed as immutable.
+    // let mutable_borrow = &mut point;
+
+    println!("Point has coordinates: ({}, {}, {})", 
+             borrowed_point.x, another_borrow.y, point.z);
+
+    let mutable_borrow = &mut point;
+
+    mutable_borrow.x = 5;
+    mutable_borrow.y = 2;
+    mutable_borrow.z = 1;
+
+    // error! can't borrow 'point' as immutable because it's currently
+    // borrowed as mutable.
+    // let y = &point.y;
+
+    // error! Can't print because 'println!' takes an immutable reference.
+    // println!("Point Z coordinate is {}", point.z);
+
+    println!("Point has coordinates: ({}, {}, {})",
+             mutable_borrow.x, mutable_borrow.y, mutable_borrow.z);
+
+    let new_borrowed_point = &point;
+    println!("Point now has coordinates: ({}, {}, {})",
+             new_borrowed_point.x, new_borrowed_point.y, new_borrowed_point.z);         
 }
