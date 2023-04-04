bb5d06412ec4c9cee8b4acc3c1524a43316c4238
jeong <chan3358@naver.com>
2023-04-04 16:31:34 +0900

    feat: add struct to scoping rules

--- rust/rust-by-example/scoping_rules/src/main.rs
+++ rust/rust-by-example/scoping_rules/src/main.rs
@@ -292,4 +292,34 @@ fn lifetimes() {
 
     owner.add_one();
     owner.print();
+
+    println!("struct");
+
+    #[derive(Debug)]
+    struct Borrowed<'a>(&'a i32);
+
+    #[derive(Debug)]
+    struct NamedBorrowed<'a> {
+        _x: &'a i32,
+        _y: &'a i32,
+    }
+
+    #[derive(Debug)]
+    enum Either<'a> {
+        Num(i32),
+        Ref(&'a i32),
+    }
+
+    let x = 18;
+    let y = 15;
+
+    let single = Borrowed(&x);
+    let double = NamedBorrowed { _x: &x, _y: &y };
+    let reference = Either::Ref(&x);
+    let number = Either::Num(y);
+
+    println!("x is borrowed in {:?}", single);
+    println!("x and y are borrowed in {:?}", double);
+    println!("x is borrowed in {:?}", reference);
+    println!("y is not borrowed in {:?}", number);
 }
