95ea06c02283f806aa1b0211d7dd9e4c541f5404
jeong <chan3358@naver.com>
2023-04-08 17:07:14 +0900

    feat: add bounds to scoping rules

--- rust/rust-by-example/scoping_rules/src/main.rs
+++ rust/rust-by-example/scoping_rules/src/main.rs
@@ -340,4 +340,26 @@ fn lifetimes() {
 
     let b: BorrowedTrait = Default::default();
     println!("b is {:?}", b);
+
+    println!("bounds");
+
+    use std::fmt::Debug;
+
+    #[derive(Debug)]
+    struct Ref<'a, T: 'a>(&'a T);
+
+    fn print<T: Debug>(t: T) {
+        println!("print: t is {:?}", t);
+    }
+
+    fn print_ref<'a, T>(t: &'a T) where
+        T: Debug + 'a {
+        println!("print_ref: t is {:?}", t);
+    }
+
+    let x = 7;
+    let ref_x = Ref(&x);
+
+    print_ref(&ref_x);
+    print(ref_x);
 }
