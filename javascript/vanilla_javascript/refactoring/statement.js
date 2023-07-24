b4f188553612353179be301ab24757ecc2b35240
seeyoujeong <chan3358@naver.com>
2023-07-24 13:48:27 +0900

    feat: add refactoring

---
+++ javascript/vanilla_javascript/refactoring/statement.js
@@ -0,0 +1,80 @@
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play);
+
+    volumeCredits += Math.max(perf.audience - 30, 0);
+
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+}
+
+function amountFor(performance, play) {
+  let result = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      result = 40000;
+      if (performance.audience > 30) {
+        result += 1000 * (performance.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (performance.audience > 20) {
+        result += 10000 + 500 * (performance.audience - 20);
+      }
+      result += 300 * performance.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+
+  return result;
+}
+
+const invoices = [
+  {
+    customer: "BigCo",
+    performances: [
+      {
+        playID: "hamlet",
+        audience: 55,
+      },
+      {
+        playID: "as-like",
+        audience: 35,
+      },
+      {
+        playID: "othello",
+        audience: 40,
+      },
+    ],
+  },
+];
+
+const plays = {
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  "as-like": { name: "As You Like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" },
+};
+
+console.log(statement(invoices[0], plays));
