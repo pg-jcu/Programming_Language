5be223e39b4260d857ced9250173b23ccbd089b2
seeyoujeong <chan3358@naver.com>
2023-07-25 15:45:22 +0900

    refactor: 임시 변수를 질의 함수로 바꾸기

--- javascript/vanilla_javascript/refactoring/statement.js
+++ javascript/vanilla_javascript/refactoring/statement.js
@@ -8,8 +8,36 @@ function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
 
+  function amountFor(performance, play) {
+    let result = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (performance.audience > 30) {
+          result += 1000 * (performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (performance.audience > 20) {
+          result += 10000 + 500 * (performance.audience - 20);
+        }
+        result += 300 * performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    return result;
+  }
+
+  function playFor(performance) {
+    return plays[performance.playID];
+  }
+
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -24,30 +52,6 @@ function statement(invoice, plays) {
 
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
-  return result;
-}
-
-function amountFor(performance, play) {
-  let result = 0;
-
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (performance.audience > 30) {
-        result += 1000 * (performance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (performance.audience > 20) {
-        result += 10000 + 500 * (performance.audience - 20);
-      }
-      result += 300 * performance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
-  }
-
   return result;
 }
 
