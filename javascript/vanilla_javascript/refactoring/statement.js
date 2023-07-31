ec5bffdaa52c9c1f90fa81819ec5e6d3a3ac039e
seeyoujeong <chan3358@naver.com>
2023-07-31 22:36:15 +0900

    refactor: renderPlainText 함수 내의 amountFor 함수 교체

--- javascript/vanilla_javascript/refactoring/statement.js
+++ javascript/vanilla_javascript/refactoring/statement.js
@@ -7,11 +7,36 @@ function statement(invoice, plays) {
   function enrichPerformance(performance) {
     const result = Object.assign({}, performance);
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(performance) {
     return plays[performance.playID];
+  }
+
+  function amountFor(performance) {
+    let result = 0;
+
+    switch (performance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (performance.audience > 30) {
+          result += 1000 * (performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (performance.audience > 20) {
+          result += 10000 + 500 * (performance.audience - 20);
+        }
+        result += 300 * performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${performance.play.type}`);
+    }
+
+    return result;
   }
 }
 
@@ -19,9 +44,7 @@ function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
 
   result += `총액: ${usd(totalAmount())}\n`;
@@ -31,7 +54,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
@@ -58,30 +81,6 @@ function renderPlainText(data, plays) {
 
     if ("comedy" === performance.play.type)
       result += Math.floor(performance.audience / 5);
-
-    return result;
-  }
-
-  function amountFor(performance) {
-    let result = 0;
-
-    switch (performance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20);
-        }
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${performance.play.type}`);
-    }
 
     return result;
   }
