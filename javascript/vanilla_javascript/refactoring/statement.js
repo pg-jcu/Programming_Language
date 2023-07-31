c98f6cd32a83353619be46dfcfadf8a0e79691a8
seeyoujeong <chan3358@naver.com>
2023-07-31 21:31:07 +0900

    refactor: totalVolumeCredits, totalAmount 함수 선언

--- javascript/vanilla_javascript/refactoring/statement.js
+++ javascript/vanilla_javascript/refactoring/statement.js
@@ -45,23 +45,32 @@ function statement(invoice, plays) {
     return result;
   }
 
-  let totalAmount = 0;
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
-
-  result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
 
