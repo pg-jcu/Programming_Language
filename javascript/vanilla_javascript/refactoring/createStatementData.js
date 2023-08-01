4a65dc1ab2ca344705bca821d3ec9553ae2b6453
seeyoujeong <chan3358@naver.com>
2023-08-01 21:47:15 +0900

    refactor: volumeCredits 메서드 선언 및 함수 인라인하기

--- javascript/vanilla_javascript/refactoring/createStatementData.js
+++ javascript/vanilla_javascript/refactoring/createStatementData.js
@@ -27,6 +27,16 @@ class PerformanceCalculator {
 
     return result;
   }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+
+    return result;
+  }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -45,22 +55,12 @@ export default function createStatementData(invoice, plays) {
     const result = Object.assign({}, performance);
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
   function playFor(performance) {
     return plays[performance.playID];
-  }
-
-  function volumeCreditsFor(performance) {
-    let result = 0;
-    result += Math.max(performance.audience - 30, 0);
-
-    if ("comedy" === performance.play.type)
-      result += Math.floor(performance.audience / 5);
-
-    return result;
   }
 
   function totalAmount(data) {
