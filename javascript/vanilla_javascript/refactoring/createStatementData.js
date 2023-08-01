e1414a0f90700a67a74bb516f41c58f8053a879e
seeyoujeong <chan3358@naver.com>
2023-08-01 21:44:18 +0900

    refactor: amount 메서드 선언 및 함수를 인라인하기

--- javascript/vanilla_javascript/refactoring/createStatementData.js
+++ javascript/vanilla_javascript/refactoring/createStatementData.js
@@ -3,6 +3,30 @@ class PerformanceCalculator {
     this.performance = performance;
     this.play = play;
   }
+
+  get amount() {
+    let result = 0;
+
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+
+    return result;
+  }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -19,38 +43,14 @@ export default function createStatementData(invoice, plays) {
       playFor(performance)
     );
     const result = Object.assign({}, performance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
   function playFor(performance) {
     return plays[performance.playID];
-  }
-
-  function amountFor(performance) {
-    let result = 0;
-
-    switch (performance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20);
-        }
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${performance.play.type}`);
-    }
-
-    return result;
   }
 
   function volumeCreditsFor(performance) {
